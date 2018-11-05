7ae43a9cd9b2321732c8faeb9d586e0796d175d3
flaviomota <flavio.belizario.mota@gmail.com>
2018-11-05 11:03:47 -0200

    Fatorial em C corrigido

--- fatorial.c
+++ fatorial.c
@@ -2,8 +2,9 @@ int main(){
 	int fat, n;
     printf("Insira um valor para o qual deseja calcular seu fatorial: ");
     scanf("%d", &n);
-    for(fat = 1; n > 1; n = n - 1)
-    fat = fat * n;
+    for(fat = 1; n > 1; n = n - 1){
+    	fat = fat * n;
+    }
     printf("\nFatorial calculado: %d", fat);
     return 0;
 }
